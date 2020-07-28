a97a8eeffde3b87e7bcf25532b7b9625a6f68c99
ZiqianXie <z.xie4@umiami.edu>
2020-07-27 19:13:34 -0500

    add utils and test

---
+++ test.py
@@ -0,0 +1,62 @@
+from utils import FormalContext
+
+
+triangle = {"T1": {'b', 'd'},
+            "T2": {'b', 'e'},
+            "T3": {'c'},
+            "T4": {'a', 'b', 'c'},
+            "T5": {'d'},
+            "T6": {'b', 'c'},
+            "T7": {'e'}}
+fc = FormalContext(triangle)
+for concept in fc.concept_generation():
+    print(concept)
+
+star_alliance = {"Air Canada": {"Latin America", "Europe", "Canada", "Asian Pacific", "Middle East", "Mexico",
+                                "Caribbean", "United States"},
+                 "Air New Zealand": {"Europe", "Asian Pacific", "United States"},
+                 "All Nippon Airways": {"Europe", "Asian Pacific", "United States"},
+                 "Ansett Australia": {"Asian Pacific"},
+                 "The Austrian Airlines Group": {"Europe", "Canada", "Asian Pacific", "Middle East", "Africa",
+                                                 "United States"},
+                 "British Midland": {"Europe"},
+                 "Lufthansa": {"Latin America", "Europe", "Canada", "Asian Pacific", "Middle East", "Africa", "Mexico",
+                               "United States"},
+                 "Mexicana": {"Latin America", "Canada", "Mexico", "Caribbean", "United States"},
+                 "Singapore Airlines": {"Europe", "Canada", "Asian Pacific", "Middle East", "Africa",
+                                           "United States"},
+                 "Scandinavian Airlines": {"Latin America", "Europe", "Asian Pacific", "Africa", "United States"},
+                 "Thai Airways International": {"Latin America", "Europe", "Asian Pacific", "Caribbean",
+                                                "United States"},
+                 "United Airlines": {"Latin America", "Europe", "Canada", "Asian Pacific", "Mexico", "Caribbean",
+                                     "United States"},
+                 "VARIG": {"Latin America", "Europe", "Asian Pacific", "Africa", "Mexico", "United States"}
+                 }
+fc1 = FormalContext(star_alliance)
+fc1.attr_closure({"Latin America", "Canada", "Middle East"})
+fc1.attributes = ["Latin America", "Europe", "Canada", "Asian Pacific", "Middle East", "Africa", "Mexico", "Caribbean",
+                  "United States"]
+fc1._next_closure({"Latin America", "Canada", "Mexico", "Caribbean", "United States"})
+len(set(fc1.concept_generation()))
+p4 = {1: {'a', 'c', 'f'},
+      2: {'b', 'd', 'f'},
+      3: {'b', 'c', 'e', 'f'},
+      4: {'b', 'c', 'f'},
+      5: {'d', 'f'},
+      6: {'a', 'b', 'c', 'e', 'f'},
+      7: {'b', 'd', 'f'}}
+fc2 = FormalContext(p4)
+fc2._next_closure({'b', 'd', 'f'})
+p5 = {1: {'b', 'c', 'e', 'f'},
+      2: {'b', 'c'},
+      3: {},
+      4: {'e', 'f'},
+      5: {'c', 'e', 'f'},
+      6: {'b'},
+      7: {'c', 'e', 'f'},
+      8: {'c', 'd', 'f'},
+      9: {'c', 'e', 'f'},
+      10: {'b', 'c', 'd', 'e', 'f'}}
+fc3 = FormalContext(p5)
+fc3.add_unused_attributes(['a'])
+len(set(fc3.concept_generation()))
---
+++ utils.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+from typing import Iterator, Union, Tuple
+
+
+class FormalContext:
+    def __init__(self, context: defaultdict(set)):
+        self.context = context
+        self.objects = set(context.keys())
+        self.context_transposed = defaultdict(set)
+        for k, v in context.items():
+            self.context_transposed[frozenset()].add(k)
+            for attr in v:
+                self.context_transposed[attr].add(k)
+        self.attributes = sorted(set(self.context_transposed.keys()).difference({frozenset()}))
+        self.context[frozenset()] = set(self.attributes)
+
+    def add_unused_attributes(self, attrs):
+        for attr in attrs:
+            self.context_transposed[attr] = set()
+            self.context[frozenset()].add(attr)
+        self.attributes = sorted(set(self.context_transposed.keys()).difference({frozenset()}))
+
+    def _closure(self, some_set: set, obj_type: bool) -> Tuple[frozenset, frozenset]:
+        if obj_type:
+            set0, set1, dict0, dict1 = set(self.attributes), set(self.objects), self.context, self.context_transposed
+        else:
+            set0, set1, dict0, dict1 = set(self.objects), set(self.attributes), self.context_transposed, self.context
+        for element in some_set.union(frozenset()):
+            set0 = set0.intersection(dict0[element])
+        for element in set0.union(frozenset()):
+            set1 = set1.intersection(dict1[element])
+        return frozenset(set1), frozenset(set0)
+
+    def obj_closure(self, objects: set) -> Tuple[frozenset, frozenset]:
+        return self._closure(objects, True)
+
+    def attr_closure(self, attributes: set) -> Tuple[frozenset, frozenset]:
+        return self._closure(attributes, False)
+
+    def _next_closure(self, A: frozenset) -> Union[Tuple[frozenset, frozenset], None]:
+        A = set(A)
+        for i in range(len(self.attributes)-1, -1, -1):
+            m = self.attributes[i]
+            if m in A:
+                A.remove(m)
+            else:
+                B, C = self.attr_closure(A.union({m}))
+                if not B.difference(A).intersection(self.attributes[:i]):
+                    return B, C
+        return None
+
+    def concept_generation(self) -> Iterator[Tuple[frozenset, frozenset]]:
+        # assume the number of objects is larger than the number of attributes
+        A = self.attr_closure(set())
+        while A is not None:
+            yield A
+            A = self._next_closure(A[0])
+
+
